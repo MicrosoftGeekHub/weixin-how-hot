0b3d8070f144ebe8c53d3691558300379f8acb2c
Sean Wu <sewu@microsoft.com>
2015-05-11 16:46:23 +0800

    save image to db

--- WeixinServer/Models/WeixinDBContext.cs
+++ WeixinServer/Models/WeixinDBContext.cs
@@ -15,6 +15,7 @@ namespace WeixinServer
         public DbSet<Danmu> Danmus { get; set; }
         public DbSet<Token> Tokens { get; set; }
         public DbSet<BlackValue> BlackList { get; set; }
+        public DbSet<ImageStorage> ImageStorages { get; set; }
     }
 
     public sealed class Danmu
@@ -25,6 +26,21 @@ namespace WeixinServer
         public string OpenId { get; set; }
         public string Content { get; set; }
         public int CreateTime { get; set; }
+    }
+
+    [Table("ImageStorages")]
+    public sealed class ImageStorage
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string OpenId { get; set; }
+        public int CreateTime { get; set; }
+        public string PicUrl { get; set; }
+        public byte[] PicContent { get; set; }
+        public string ParsedUrl { get; set; }
+        public byte[] ParsedContent { get; set; }
+        public string ParsedDescription { get; set; }
     }
 
     public sealed class Token
